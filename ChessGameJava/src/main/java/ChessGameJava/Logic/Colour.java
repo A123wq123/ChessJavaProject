383ab171321eadacfed4f827b9f53723569ebcc2
A123wq123 <charles.degrandpre@polymtl.ca>
2023-09-03 01:50:55 -0400

    Updated documentation for Colour

--- ChessGameJava/src/main/java/ChessGameJava/Logic/Colour.java
+++ ChessGameJava/src/main/java/ChessGameJava/Logic/Colour.java
@@ -5,6 +5,7 @@ package ChessGameJava.Logic;
  * BLACK : a black coloured piece
  * WHITE : a white coloured piece.
  * NULL : an instance of the NullPiece class.
+ * 
  * @Author Charles Degrandpré
  * @Last_Updated 2022-12-23
  */
