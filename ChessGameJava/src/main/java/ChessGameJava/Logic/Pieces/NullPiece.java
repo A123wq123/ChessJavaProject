86115207399d3c19f8696556f740c57166e1579d
A123wq123 <charles.degrandpre@polymtl.ca>
2023-09-02 20:08:34 -0400

    Updated documentation for NullPiece and refactored code

--- ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/NullPiece.java
+++ ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/NullPiece.java
@@ -8,8 +8,14 @@ import ChessGameJava.Logic.Moves.ChessABSMove;
 import java.util.ArrayList;
 
 /**
- * This class acts as a null piece, so a piece that does nothing. This class is
+ * This class acts as a null piece, in other words a piece that does nothing. This class is
  * usefully within the architecture of this project as all squares require pieces.
+ * 
+ * This allows us to interface with squares without fear of there not being a piece. But 
+ * rather a piece that returns empty lists and values clearly stated as null. 
+ * 
+ * All instances of this class have the Colour of NULL.
+ * 
  * @Author Charles Degrandpré
  * @Last_Updated 2022-12-23
  */
@@ -17,22 +23,19 @@ public class NullPiece extends ChessABSPieceModel {
     /**
      * Constructor of a null piece. as the piece is meant to be
      * unable to do anything, it doesn't have parameters.
+     * 
+     * It sets the Colour of the piece to NULL. Useful when checking 
+     * if a square is occupied. If the Colour is NULL, then the square can
+     * be considered empty. 
      */
     public NullPiece() {
         this.colour = Colour.NULL;
     }
 
     /**
-     * Returns the list of moves a null piece is capable of doing, which in
-     * this case is none.
-     * @param currentPos the current position of the piece.
-     * @return the list of moves the piece is capable of doing.
+     * Method used to return the list of squares a NullPiece may attack. Always
+     * returns a new empty ArrayList of ChessSquareModel. 
      */
-    @Override
-    public ArrayList<ChessABSMove> getListMoves(ChessSquareModel currentSquare, ChessBoardModel board) {
-        return new ArrayList<>();
-    }
-
     @Override
     public ArrayList<ChessSquareModel> getListAttackingSquares(ChessSquareModel currentSquare, ChessBoardModel board) {
         return new ArrayList<>();
