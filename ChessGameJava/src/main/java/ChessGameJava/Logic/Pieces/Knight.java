4e96af4a7e1631096d31886e4fd90b128611ac6c
A123wq123 <charles.degrandpre@polymtl.ca>
2023-09-02 20:04:20 -0400

    Updated documentation for Knight and refactored code

--- ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/Knight.java
+++ ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/Knight.java
@@ -10,22 +10,30 @@ import ChessGameJava.Utility.Position;
 
 /**
  * The class representing a knight within the project. This class is capable of returning all
- * possible moves that the knight can do.
+ * possible moves that the knight can do and all the squares it can attack.
+ * 
  * @Author Charles Degrandpré
- * @Last_Updated 2023-08-30
+ * @Last_Updated 2023-09-02
  */
 public class Knight extends ChessABSPieceModel {
 
     /**
-     * Constructor of the Knight class. Assigns the board as well as the colour of the piece.
-     * @param board the instance of the chess board.
+     * Constructor of the Knight class. Assigns the colour of the piece.
+     * 
      * @param colourOfPiece the colour of the knight.
      */
     public Knight(Colour colourOfPiece) {
-        super();
         this.colour = colourOfPiece;
     }
 
+    /**
+     * Method used to return all the squares a knight can attack. A knight can attack all all 
+     * squares that are two squares away from him in all cardinal directions (so two squares up, 
+     * two squares down, two squares right and tqo squares left) and one square away in a direction
+     * perpendicular to the direction of the two squares previously mentioned giving a sort of
+     * L shape. The squares can be attacked regardless of if the path is blocked as the knight 
+     * can jump over pieces. 
+     */
     @Override
     public ArrayList<ChessSquareModel> getListAttackingSquares(ChessSquareModel currentSquare, ChessBoardModel board) {
         ArrayList<ChessSquareModel> listSquares = new ArrayList<>();
