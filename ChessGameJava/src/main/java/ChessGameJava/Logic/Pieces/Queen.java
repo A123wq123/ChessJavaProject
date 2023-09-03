db053b5e52171902d2370cbce2276e9e0dbe58d6
A123wq123 <charles.degrandpre@polymtl.ca>
2023-09-02 21:31:46 -0400

    Updated documentation for Queen and refactored code

--- ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/Queen.java
+++ ChessGameJava/src/main/java/ChessGameJava/Logic/Pieces/Queen.java
@@ -10,21 +10,27 @@ import ChessGameJava.Utility.MoveCalculatorHelper.DIRECTION;
 
 /**
  * The class representing a queen within the project. This class is capable of returning all
- * possible moves that the queen can do.
+ * possible moves that the queen can do and all the squares it can attack. 
+ * 
  * @Author Charles Degrandpré
- * @Last_Updated 2023-08-29
+ * @Last_Updated 2023-09-02
  */
 public class Queen extends ChessABSPieceModel {
     /**
-     * Constructor of the queen class. Assigns the board as well as the colour of the piece.
+     * Constructor of the queen class. Assigns the colour of the piece.
+     * 
      * @param board the instance of the chess board.
      * @param colourOfPiece the colour of the queen.
      */
     public Queen(Colour colourOfPiece) {
-        super();
         this.colour = colourOfPiece;
     }
 
+    /**
+     * Method used to return all the squares a queen can attack. A queen can attack all 
+     * squares diagonal to her current square and all squares in any of the four cardinal 
+     * directions to her current square up to and including the first occupied square. 
+     */
     @Override
     public ArrayList<ChessSquareModel> getListAttackingSquares(ChessSquareModel currentSquare, ChessBoardModel board) {
         ArrayList<ChessSquareModel> listSquares = new ArrayList<>();
